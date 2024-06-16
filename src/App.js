278e7218973b03d9340d628ae1a83666776d22f6
langat68 <langatfidel32gmail.com>
2024-06-16 19:16:58 +0300

    using math.random library

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // down here, i added a function that has a variable that takes three names and  math.random library that will alternate the names 
+  // when the function is called
+  const handleNameChange =() => { 
+    const names = ['Fidel',['Kipsang'],["Langat"]];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          There's more to what meets the eye😂😁
         </p>
         <a
           className="App-link"
@@ -17,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Hello {handleNameChange() /*we are calling the function we defined */}</p> 
       </header>
     </div>
   );
